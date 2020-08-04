86768699291e18cdc519a92b2141cf95f8b8c343
张琳(952999) <952999@nd.com>
2020-08-04 09:42:59 +0800

    SynchronousQueue、TransferQueue DEMO补充

--- JUC/src/main/java/com/mashibing/juc/c_025/T08_SynchronusQueue.java
+++ JUC/src/main/java/com/mashibing/juc/c_025/T08_SynchronusQueue.java
@@ -3,10 +3,13 @@ package com.mashibing.juc.c_025;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.SynchronousQueue;
 
+/**
+ *SynchronusQueue内部没有单独维护一个队列，只能使用put方法，TransferQueue同理，只能使用transfer、put方法。
+ */
 public class T08_SynchronusQueue { //容量为0
 	public static void main(String[] args) throws InterruptedException {
 		BlockingQueue<String> strs = new SynchronousQueue<>();
-		
+
 		new Thread(()->{
 			try {
 				System.out.println(strs.take());
@@ -17,7 +20,24 @@ public class T08_SynchronusQueue { //容量为0
 
 		strs.put("aaa"); //阻塞等待消费者消费
 		//strs.put("bbb");
-		//strs.add("aaa");
+//		strs.add("aaa");// Queue full
 		System.out.println(strs.size());
+
+		/************************* demo2 *************************/
+		new Thread(()->{
+			try {
+				System.out.println(strs.take());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+		new Thread(()->{
+			try {
+				strs.put("aaa");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
 	}
 }
--- JUC/src/main/java/com/mashibing/juc/c_025/T09_TransferQueue.java
+++ JUC/src/main/java/com/mashibing/juc/c_025/T09_TransferQueue.java
@@ -1,11 +1,33 @@
 package com.mashibing.juc.c_025;
 
 import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
 
 public class T09_TransferQueue {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws Exception {
+		method1();
+		method2();
+		method3();
+	}
+
+	private static void method1() throws Exception {
 		LinkedTransferQueue<String> strs = new LinkedTransferQueue<>();
-		
+
+		new Thread(() -> {
+			try {
+				System.out.println(strs.take());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+		System.out.println(strs.tryTransfer("aaa", 1, TimeUnit.SECONDS));
+	}
+
+	private static void method2() throws Exception {
+		LinkedTransferQueue<String> strs = new LinkedTransferQueue<>();
+
+//		strs.transfer("aaa"); // 阻塞等待消费交换
+
 		new Thread(() -> {
 			try {
 				System.out.println(strs.take());
@@ -14,15 +36,35 @@ public class T09_TransferQueue {
 			}
 		}).start();
 		strs.transfer("aaa");
-		
-		//strs.put("aaa");
+//		System.out.println(strs.tryTransfer("aaa", 1, TimeUnit.SECONDS));
 
-		/*new Thread(() -> {
+//		strs.put("aaa");
+
+//		strs.tryTransfer("aaa");
+	}
+
+	private static void method3() throws Exception {
+		LinkedTransferQueue<String> strs = new LinkedTransferQueue<>();
+		new Thread(() -> {
 			try {
 				System.out.println(strs.take());
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		}).start();*/
+		}).start();
+
+		new Thread(() -> {
+			try {
+				System.out.println(strs.take());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+		strs.transfer("aaa");
+		if (strs.hasWaitingConsumer()) {
+			System.out.println("消费者个数 : " + strs.getWaitingConsumerCount());
+			strs.transfer("aaa");
+		}
 	}
 }
